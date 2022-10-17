58d78b0da08b14bf0f9cc5e6ce30a5e525a6f0d9
Biancavdvoort <115459582+Biancavdvoort@users.noreply.github.com>
2022-10-17 22:36:34 +0200

    Rewrote the Javascript temperature input to make it more readable.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
-
-function showTemperature(response) {
-  let cityElement = document.querySelector("city");
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-conditions");
+  let feelsLikeElement = document.querySelector("#feels-like");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let feelElement = document.querySelector("#feels-like");
-  let dateElement = document.querySelector(".current-time");
 
-  cityElement.innerHTML = response.data.name;
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  feelElement.innerHTML = Math.round(response.data.main.feels_like);
-  dateElement.innerHTML = formatDate(currentTime);
 }
 
-let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
-let city = "Amsterdam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiKey = "47ctcdaecb3f4bd83044o3bbac34f29d";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Amsterdam&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
