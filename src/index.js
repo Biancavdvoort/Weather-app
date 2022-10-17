1c62e8f25d58e0969355e39e8f38ed9e16e2c7f3
Biancavdvoort <115459582+Biancavdvoort@users.noreply.github.com>
2022-10-17 21:34:47 +0200

    Cleaning up the code, making it more readable and debugging.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,7 +9,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -18,45 +18,28 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
-  return `${days[dayIndex]} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-
-let dateElement = document.querySelector(".current-time");
-let currentTime = new Date();
-
-dateElement.innerHTML = formatDate(currentTime);
 
 function showTemperature(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  console.log(response.data);
-  document.querySelector("#weather-conditions").innerHTML =
-    response.data.weather[0].description;
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+  let cityElement = document.querySelector("city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#weather-conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelElement = document.querySelector("#feels-like");
+  let dateElement = document.querySelector(".current-time");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateElement.innerHTML = formatDate(currentTime);
 }
 
-function search(city) {
-  let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  search(city);
-}
-
-let form = document.querySelector("#search-input");
-form.addEventListener("submit", handleSubmit);
-
-search("Amsterdam");
+let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
+let city = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
