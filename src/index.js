73d9df72452b11ff764d7fb69f1126313d520fb7
Biancavdvoort <115459582+Biancavdvoort@users.noreply.github.com>
2022-10-18 20:00:36 +0200

    Added the search engine.

--- src/index.js
+++ src/index.js
@@ -45,8 +45,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "47ctcdaecb3f4bd83044o3bbac34f29d";
-let city = "Miami";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+function search(city) {
+  let apiKey = "47ctcdaecb3f4bd83044o3bbac34f29d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Miami");
+
+let form = document.querySelector("#search-input");
+form.addEventListener("submit", handleSubmit);
